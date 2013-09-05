41f9d74e97d803eaa8ba9c9e158ae02a971360f5
David Keegan <dksw@eircom.net>
2013-09-05 23:16:40 +0100

    Melpa Build.

--- dkdo-pkg.el
+++ dkdo-pkg.el
@@ -1,3 +1,3 @@
-(define-package "dkdo" "0.3"
+(define-package "dkdo" "0.4"
  "Do List mode using some features of org-mode."
- '((dkmisc "0.3") (org "20130826")))
+ '((dkmisc "0.4") (org "20130826")))
