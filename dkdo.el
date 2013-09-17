087dc02756eb52270dae41c3baee60d4fe3b9288
David Keegan <dksw@eircom.net>
2013-09-17 22:53:31 +0100

    Misc.

--- dkdo.el
+++ dkdo.el
@@ -10,7 +10,7 @@
 
 ;;; Commentary:
 ;;
-;; This is a Do List mode for a user who needs to manipulate the
+;; This is a Do List mode for a user who likes to manipulate the
 ;; entries directly to adjust priorities. It uses org/outline mode to
 ;; make it easy to get an overview. Urgent tasks appear near the top
 ;; of the file, and can be made to move automatically to the top of
